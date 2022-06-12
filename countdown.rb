54caa89492593cd0e936408db3802bee150f5928
Luke <luciusfow@gmail.com>
2022-06-12 13:58:29 -0400

    commit tree b1e7f2a503cabaccba10e3317c79b01273191ec6

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    while x > 0 
+        puts "#{x} SECOND(S)!"
+        x -=1
+        
+    end
+     "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+    while x > 0 
+        puts "#{x} SECOND(S)!"
+        sleep(1) #sleep causes code to sleep for 1 second
+        x -=1
+    end
+    "HAPPY NEW YEAR!"
 end
